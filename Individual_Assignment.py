a4300475e49d4cde51fd2345db7e2f7122ba7221
Rachel van Grootheest <rachelvg12@gmail.com>
2020-01-16 14:34:56 +0100

    changed names of variables so they look better in R

--- Individual_Assignment.py
+++ Individual_Assignment.py
@@ -4,10 +4,10 @@ import json
 with open('Yemen_2011.json', encoding='utf-16') as file:
     conflicts = json.load(file)
 
-with open('Yemen_2011.csv', 'w', encoding = 'utf-16') as file:
-    file.writelines('source and event,type of violence,amount of deaths (best)\n')
+with open('Yemen_2011.csv', 'w') as file:
+    file.writelines('id,type_of_violence,deaths\n')
     for conflict in conflicts:
-        file.writelines(f'{conflict["source_article"]}, {conflict["type_of_violence"]}, {conflict["best"]}\n')
+        file.writelines(f'{conflict["id"]}, {conflict["type_of_violence"]}, {conflict["best"]}\n')
 
 
 #{conflicts['deaths_]}, {conflicts['deaths_b']}, {conflicts['deaths_civilians']}, {conflicts['deaths_unknown']},
