9dbdf34fae5f26fb42b1d865d3f32503f532cfce
Rachel van Grootheest <rachelvg12@gmail.com>
2020-01-16 12:17:29 +0100

    Created column names for variables

--- Individual_Assignment.py
+++ Individual_Assignment.py
@@ -1,7 +1,15 @@
 import json 
 
 #load file 
-with open('Yemen_2011.json') as file:
-    conflicts = file.read()
+with open('Yemen_2011.json', encoding='utf-16') as file:
+    conflicts = json.load(file)
 
-print(conflicts)
+with open('Yemen_2011.csv', 'w', encoding = 'utf-16') as file:
+    file.writelines('source and event,type of violence,amount of deaths (best)\n')
+    for conflict in conflicts:
+        file.writelines(f'{conflict["source_article"]}, {conflict["type_of_violence"]}, {conflict["best"]}\n')
+
+
+#{conflicts['deaths_]}, {conflicts['deaths_b']}, {conflicts['deaths_civilians']}, {conflicts['deaths_unknown']},
+
+print(conflict)
